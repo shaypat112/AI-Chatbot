6777b3182312d665a970b8cedb55ae74c794a515
shaypat112 <shivangpatel2050@gmail.com>
2025-08-03 12:31:11 -0400

    Initial commit

--- app.py
+++ app.py
@@ -33,3 +33,8 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Heroku sets PORT env variable
+    app.run(host="0.0.0.0", port=port)
